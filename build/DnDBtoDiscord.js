f2d34604abdcd109daa635ed41d5bca50c345af6
Andrew J Kennedy <andrew.j.kennedy@gmail.com>
2020-04-30 17:54:01 +1000

    Tray agin x3

--- build/DnDBtoDiscord.js
+++ build/DnDBtoDiscord.js
@@ -1,11 +1,22 @@
 // ==UserScript==
-// @name         DnDBtoDiscord
-// @namespace    http://tampermonkey.net/
-// @version      0.2.1
-// @description  DnD Beyond to Discord Integration
-// @author       Andrew J Kennedy
+// @namespace     https://openuserjs.org/users/Andrew-J-Kennedy
+// @name          DnDBtoDiscord
+// @description   DnD Beyond to Discord Integration
+// @copyright     2020, Andrew-J-Kennedy (https://openuserjs.org/users/Andrew-J-Kennedy)
+// @license       OSI-SPDX-Short-Identifier
+// @version       0.2.1
 // @match        https://www.dndbeyond.com/encounters/*
 // @match        https://www.dndbeyond.com/profile/*/characters/*
-// @grant        none
 // @require      file://C:/DnDBtoDiscordPrivate.js
+// @grant none
 // ==/UserScript==
+
+// ==OpenUserJS==
+// @author Andrew-J-Kennedy
+// ==/OpenUserJS==
+
+/**
+  *
+  * Please begin typing or paste your User script now.
+  *
+  */
