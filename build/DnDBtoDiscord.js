dbca2050885e02c8a84624fef8a38f7982628d27
Andrew J Kennedy <andrew.j.kennedy@gmail.com>
2020-04-30 18:02:12 +1000

    Tray agin x 4

--- build/DnDBtoDiscord.js
+++ build/DnDBtoDiscord.js
@@ -3,11 +3,11 @@
 // @name          DnDBtoDiscord
 // @description   DnD Beyond to Discord Integration
 // @copyright     2020, Andrew-J-Kennedy (https://openuserjs.org/users/Andrew-J-Kennedy)
-// @license       OSI-SPDX-Short-Identifier
+// @license       MIT
 // @version       0.2.1
-// @match        https://www.dndbeyond.com/encounters/*
-// @match        https://www.dndbeyond.com/profile/*/characters/*
-// @require      file://C:/DnDBtoDiscordPrivate.js
+// @match         https://www.dndbeyond.com/encounters/*
+// @match         https://www.dndbeyond.com/profile/*/characters/*
+// @require       file://C:/DnDBtoDiscordPrivate.js
 // @grant none
 // ==/UserScript==
 
@@ -20,3 +20,4 @@
   * Please begin typing or paste your User script now.
   *
   */
+ 
