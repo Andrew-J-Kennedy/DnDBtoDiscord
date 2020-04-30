a98d613dffe4639d4cd22060c7a5bfc8ad656c36
Andrew J Kennedy <andrew.j.kennedy@gmail.com>
2020-04-30 18:38:43 +1000

    Try Again x 6

--- DnDBtoDiscordTampermonkyHeader.js
+++ DnDBtoDiscordTampermonkyHeader.js
@@ -1,12 +1,17 @@
 // ==UserScript==
-// @name         DnDBtoDiscord
-// @namespace    http://tampermonkey.net/
-// @version      0.2.1
-// @description  DnD Beyond to Discord Integration
-// @author       Andrew J Kennedy
-// @match        https://www.dndbeyond.com/encounters/*
-// @match        https://www.dndbeyond.com/profile/*/characters/*
-// @grant        none
-// @require      file:///home/andrew/git/DnDBtoDiscord/DnDBtoDiscordPrivate.js
-// @require      file:///home/andrew/git/DnDBtoDiscord/DnDBtoDiscord.js
+// @namespace     https://openuserjs.org/users/Andrew-J-Kennedy
+// @name          DnDBtoDiscord
+// @description   DnD Beyond to Discord Integration
+// @copyright     2020, Andrew-J-Kennedy (https://openuserjs.org/users/Andrew-J-Kennedy)
+// @license       MIT
+// @version       0.2.3
+// @match         https://www.dndbeyond.com/encounters/*
+// @match         https://www.dndbeyond.com/profile/*/characters/*
+// @require       file:///home/andrew/git/DnDBtoDiscord/DnDBtoDiscordPrivate.js
+// @require       file:///home/andrew/git/DnDBtoDiscord/DnDBtoDiscord.js
+// @grant none
 // ==/UserScript==
+
+// ==OpenUserJS==
+// @author Andrew-J-Kennedy
+// ==/OpenUserJS==
