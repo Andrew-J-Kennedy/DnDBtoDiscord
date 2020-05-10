53368d4abdbb0001528f447bd7ba4599aaf55db1
Andrew J Kennedy <andrew.j.kennedy@gmail.com>
2020-05-10 18:58:32 +1000

    0.3.2.8 Troubleshooting Del @req GM_config

--- build/DnDBtoDiscord.js
+++ build/DnDBtoDiscord.js
@@ -5,11 +5,10 @@
 // @author        Andrew-J-Kennedy
 // @copyright     2020, Andrew-J-Kennedy (https://openuserjs.org/users/Andrew-J-Kennedy)
 // @license       MIT
-// @version       0.3.2.7
+// @version       0.3.2.8
 // @match         https://www.dndbeyond.com/encounters/*
 // @match         https://www.dndbeyond.com/profile/*/characters/*
 // @match         https://www.dndbeyond.com/characters/*
-// @require       https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @grant         GM_getValue
 // @grant         GM_setValue
 // @require       file:///opt/DnDBtoDiscord/DnDBtoDiscordPrivate.js
