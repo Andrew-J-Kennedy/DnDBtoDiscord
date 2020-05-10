70e687a7e6dd69c36af8785451d8f73a36f61a20
Andrew J Kennedy <andrew.j.kennedy@gmail.com>
2020-05-10 18:53:22 +1000

    0.3.2.6 Troubleshooting Add new @requires

--- TampermonkeyHeader.js
+++ TampermonkeyHeader.js
@@ -5,10 +5,13 @@
 // @author        Andrew-J-Kennedy
 // @copyright     2020, Andrew-J-Kennedy (https://openuserjs.org/users/Andrew-J-Kennedy)
 // @license       MIT
-// @version       0.3.2.5
+// @version       0.3.2.6
 // @match         https://www.dndbeyond.com/encounters/*
 // @match         https://www.dndbeyond.com/profile/*/characters/*
 // @match         https://www.dndbeyond.com/characters/*
+// @require       https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @grant         GM_getValue
+// @grant         GM_setValue
 // @require       file:///opt/DnDBtoDiscord/DnDBtoDiscordPrivate.js
 
 // @grant none
