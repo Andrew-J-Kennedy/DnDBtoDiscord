2eccbd7db594881b55940a2e80bb114f31a59c5a
Andrew J Kennedy <andrew.j.kennedy@gmail.com>
2020-05-10 19:19:43 +1000

    0.3.2.14

--- TampermonkeyHeader.js
+++ TampermonkeyHeader.js
@@ -3,7 +3,6 @@
 // @name          DnDBtoDiscord
 // @description   DnD Beyond to Discord Integration
 // @author        Andrew-J-Kennedy
-// @copyright     2020, Andrew-J-Kennedy (https://openuserjs.org/users/Andrew-J-Kennedy)
 // @license       LGPL-3.0-or-later; https://raw.githubusercontent.com/Andrew-J-Kennedy/DnDBtoDiscord/master/LICENSE
 // @version       0.3.2.11
 // @match         https://www.dndbeyond.com/encounters/*
