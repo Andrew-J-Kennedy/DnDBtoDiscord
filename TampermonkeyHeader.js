7b05feadbadb7e73e3f45dc1912443d8b0cee143
Andrew J Kennedy <andrew.j.kennedy@gmail.com>
2020-05-10 18:44:30 +1000

    0.3.2.2 Troubleshooting Same Header

--- TampermonkeyHeader.js
+++ TampermonkeyHeader.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @namespace     https://openuserjs.org/users/Andrew-J-Kennedy
-// @name          DnDBtoDiscord-Dev
+// @name          DnDBtoDiscord
 // @description   DnD Beyond to Discord Integration
 // @author        Andrew-J-Kennedy
 // @copyright     2020, Andrew-J-Kennedy (https://openuserjs.org/users/Andrew-J-Kennedy)
 // @license       MIT
-// @version       0.3.5
+// @version       0.3.2.2
 // @match         https://www.dndbeyond.com/encounters/*
 // @match         https://www.dndbeyond.com/profile/*/characters/*
 // @match         https://www.dndbeyond.com/characters/*
-// @require       https://openuserjs.org/src/libs/sizzle/GM_config.js
-// @grant         GM_getValue
-// @grant         GM_setValue
 // @require       file:///opt/DnDBtoDiscord/DnDBtoDiscordPrivate.js
+// @require       file://C:/DnDBtoDiscordPrivate.js
+// @require       file://C:/temp/DnDBtoDiscordPrivate.js
+// @require       DnDBtoDiscordConfig.js
 
 // @grant none
 // ==/UserScript==
